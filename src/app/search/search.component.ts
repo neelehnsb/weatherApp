f3e9e118011d74c46f8340d6469f1b6a478e22b0
Your Name <19uec104@lnmiit.ac.in>
2023-08-14 15:38:31 +0530

    wip

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { OpencageGeocodingService } from '../opencage-geocoding.service';
 import { Router } from '@angular/router';
 
+export let geocodingResponse:any={}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,12 +11,14 @@ import { Router } from '@angular/router';
 })
 export class SearchComponent {
   location: string = '';
+  
 
   constructor(private opencageGeocodingService: OpencageGeocodingService, private router: Router) {}
 
   async searchLocation() {
     try {
-      const geocodingResponse = await this.opencageGeocodingService.getGeocode(this.location);
+      geocodingResponse = await this.opencageGeocodingService.getGeocode(this.location);
+      console.log(geocodingResponse.results)
       const { lat, lng } = geocodingResponse.results[0].geometry;
       this.router.navigate(['/weather'], { queryParams: { lat, lon: lng } });
     } catch (error) {
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -5,30 +5,172 @@ import { WeatherService } from '../weather.service';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
-  currentWeather: any;
-  forecast: any;
+  currentWeather: WeatherData = new WeatherData({});
+  forecastData: any;
 
-  constructor(private route: ActivatedRoute, private weatherService: WeatherService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private weatherService: WeatherService
+  ) {}
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
       const lat = params['lat'];
       const lon = params['lon'];
 
-      if (lat && lon) {
-        this.weatherService.getCurrentWeather(lat, lon)
-          .subscribe(currentWeather => {
-            this.currentWeather = currentWeather;
-          });
+      // Fetch current weather data based on lat and lon
+      this.weatherService.getCurrentWeather(lat, lon).subscribe(
+        (data) => {
+          this.currentWeather = new WeatherData(data)
+          console.log(this.currentWeather)
+        },
+        (error) => {
+          console.error('Error fetching current weather data:', error);
+        }
+      );
 
-        this.weatherService.getForecast(lat, lon)
-          .subscribe(forecast => {
-            this.forecast = forecast;
-          });
-      }
+      // Fetch forecast data based on lat and lon
+      this.weatherService.getForecast(lat, lon).subscribe(
+        (data) => {
+          this.forecastData = data.list;
+          console.log(this.forecastData)
+        },
+        (error) => {
+          console.error('Error fetching forecast data:', error);
+        }
+      );
     });
   }
+
+  formatDate(unixTimestamp: number): string {
+    const date = new Date(unixTimestamp * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      hour:'numeric',
+      minute:'numeric'
+    };
+    return date.toLocaleDateString('en-US', options);
+  }
 }
+
+export class WeatherData {
+  coord: Coord
+  weather: Weather[]
+  base: string
+  main: Main
+  visibility: number
+  wind: Wind
+  clouds: Clouds
+  dt: number
+  sys: Sys
+  timezone: number
+  id: number
+  name: string
+  cod: number
+
+  constructor(data:any){
+    data=data||{}
+    this.base=data.base||''
+    this.clouds= new Clouds(data.clouds)||new Clouds({})
+    this.cod=data.cod||-1
+    this.coord= new Coord(data.coord)||new Coord({})
+    this.dt=data.dt||-1
+    this.id=data.id||-1
+    this.main=new Main(data.main)||new Main({})
+    this.name= data.name||''
+    this.sys= new Sys(data.sys)||new Sys({})
+    this.timezone=data.timezone||-1
+    this.visibility=data.visibility||-1
+    this.weather= data?.weather?.map((item:Weather)=>new Weather(item)) || [new Weather({})]
+    this.wind=new Wind(data.wind)||new Wind({})
+  }
+}
+
+export class Coord {
+  lon: number
+  lat: number
+  constructor(data:any){
+    data=data||{}
+    this.lon=data.lon||-1
+    this.lat=data.lat||-1
+  }
+}
+
+
+export class Weather {
+  id: number
+  main: string
+  description: string
+  icon: string
+  constructor(data:any){
+    data=data||{}
+    this.description=data.description||''
+    this.icon=data.icon||''
+    this.id=data.id||-1
+    this.main=data.main||''
+  }
+}
+
+export class Main {
+  temp: number
+  feels_like: number
+  temp_min: number
+  temp_max: number
+  pressure: number
+  humidity: number
+  sea_level: number
+  grnd_level: number
+  constructor(data:any){
+    data=data||{}
+    this.feels_like=data.feels_like||-1
+    this.grnd_level=data.grnd_level||-1
+    this.humidity=data.humidity||-1
+    this.pressure=data.pressure||-1
+    this.sea_level=data.sea_level||-1
+    this.temp=data.temp||-1
+    this.temp_max=data.temp_max||-1
+    this.temp_min=data.temp_min||-1
+  }
+}
+
+export class Wind {
+  speed: number
+  deg: number
+  gust: number
+  constructor(data:any){
+    data=data||{}
+    this.deg=data.deg||-1
+    this.gust=data.gust||-1
+    this.speed=data.speed||-1
+  }
+}
+
+export class Clouds {
+  all: number
+  constructor(data:any){
+    data=data||{}
+    this.all=data.all||-1
+  }
+}
+
+export class Sys {
+  type: number
+  id: number
+  country: string
+  sunrise: number
+  sunset: number
+  constructor(data:any){
+    data=data||{}
+    this.country=data.country||''
+    this.id=data.id||-1
+    this.sunrise=data.sunrise||-1
+    this.sunset=data.sunset||-1
+    this.type=data.type||-1
+  }
+}
+
